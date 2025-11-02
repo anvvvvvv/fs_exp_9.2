41edc55f4d5b053c7bbb5b3d35de865bb09f9113
anvvvvvv <anvesha250705@gmail.com>
2025-11-02 22:16:49 +0530

    App.js

---
+++ my-react-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>FS_EXP_9.2 – CI/CD with GitHub Actions</h1>
+      <p>✅ This React app is automatically built and tested using GitHub Actions!</p>
+    </div>
+  );
+}
+
+export default App;
